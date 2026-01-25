0a3080f4c84351bfeb3f8887e76aeb15fad6a891
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-25 09:56:29 +0530

    PROGRAMMING HELLO WORLD

---
+++ Basic_syntax.java
@@ -0,0 +1,8 @@
+//Write a program to print “Hello World”
+public class Hello{
+  public static void main(String[]args){
+    System.out.println("HELLO WORLD");
+    System.out.println("IAM LEARNING JAVA");
+    System.out.println("IAM LEARNING DSA");
+  }
+}
