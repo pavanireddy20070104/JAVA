abab3f16c59451814b87b2a479569cfb043d7b51
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-26 10:01:38 +0530

    Simple calculator using switch

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+//Simple calculator using switch
+import java.util.Scanner;
+public class Hello{
+  public static void main(String[]args){
+   Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+   double num1 = sc.nextDouble();
+   System.out.print("Enter second number: ");
+   double num2 = sc.nextDouble();
+   System.out.print("Choose an operator (+, -, *, /): ");
+   char op=sc.next().charAt(0);
+   switch(op) {
+            case '+':
+                System.out.println("Result = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result = " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println("Result = " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+
+        sc.close();
+      }
+  }
