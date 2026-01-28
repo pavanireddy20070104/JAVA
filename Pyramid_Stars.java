42bc71847a94a11c9b9fcdd594b7b80dc9c333ff
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-28 20:24:14 +0530

    PRINTING STARS IN THE FORM OF PYRAMID

---
+++ Pyramid_Stars.java
@@ -0,0 +1,8 @@
+public class Pyramid_Stars{
+    public static void main(String[]args){
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    }
+}
